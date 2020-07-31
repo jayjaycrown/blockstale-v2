97e9529bb468e863a4be75f492c998798e533dff
jayjaycrown <olajidecrown@gmail.com>
2020-07-31 15:47:01 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -13,18 +13,4 @@ res.sendFile(path.join(__dirname+'/dist/browser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
