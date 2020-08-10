2041127e24fecfcabef67b13615667c29888b5fd
jayjaycrown <olajidecrown@gmail.com>
2020-08-10 02:35:12 +0100

    Minor auth UI fixes

--- src/app/pages/response-reset/response-reset.component.ts
+++ src/app/pages/response-reset/response-reset.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-response-reset',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResponseResetComponent implements OnInit {
 
-  constructor() { }
+  serverErrorMessages;
+  successMessage;
+  model = {
+    otp: ''
+  };
+  resetModel = {
+    password: ''
+  };
+  response;
+  check = true;
+  reset;
+  OTP;
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    this.OTP = form.value.otp;
+    this.auth.checkOtp(form.value.otp).subscribe(res => {
+      this.response = res;
+      if (this.response.status === 200) {
+        this.successMessage = this.response.message;
+        setTimeout(() => {
+          this.check = false;
+          this.reset = true;
+        }, 2000);
+      }
+      else {
+        this.serverErrorMessages = this.response.message;
+      }
+
+      console.log(res);
+    }, err => {
+        this.serverErrorMessages = err;
+        console.log(err);
+    });
+  }
+
+  OnReset(reset: NgForm) {
+    this.auth.resetPassword(this.OTP, reset.value).subscribe(res => {
+      this.successMessage = res;
+      setTimeout(() => {
+        this.router.navigateByUrl('auth');
+      }, 5000);
+    }, err => {
+        this.serverErrorMessages = err;
+    });
   }
 
 }
--- src/app/pages/verify/verify.component.ts
+++ src/app/pages/verify/verify.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-verify',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerifyComponent implements OnInit {
 
-  constructor() { }
+  serverErrorMessages;
+  successMessage;
+  model = {
+    verify: ''
+  };
+  response;
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    this.auth.verify(form.value.verify).subscribe(res => {
+      this.response = res;
+      if (this.response.status === 200) {
+        this.successMessage = this.response.message;
+        setTimeout(() => {
+          this.router.navigate(['auth']);
+        }, 3000);
+      }
+      else {
+        this.serverErrorMessages = this.response.message;
+      }
+    }, err => {
+        this.serverErrorMessages = err.message;
+    });
   }
 
 }
